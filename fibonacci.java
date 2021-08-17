27fdf7e856d8820b90b70ce6cb701b1e9b67cf76
IAmMADMAX <75090307+CoderMADMAX@users.noreply.github.com>
2021-08-17 11:46:32 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class fibonacci 
+{
+  public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	int a,f=0,s=1;
+	System.out.println("Enter the number of elements: ");
+	a=sc.nextInt();
+	if(a == 0)
+	System.out.println("Fibonacci series has zero elements.");	
+	else if(a == 1) 
+	System.out.println("Fibonacci series is : "+ f);
+	else if(a==2)  
+	System.out.println("Fibonacci series is : "+f + s);
+	else 
+	{
+	System.out.println("Fibonacci series is : " +f + s);
+	while(a>2)
+	{
+	 s=s+f;
+	 f=s-f;
+	System.out.println(" " +s);
+	a--;
+	}
+      }
+  }
+}
